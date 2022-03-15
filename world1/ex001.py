d8396a2a18100541a3de901d8458bdcf2ffdfbac
elianderson-dev <nin2333124568@gmail.com>
2022-03-15 13:02:17 +0000

    first commit

---
+++ world1/ex001.py
@@ -0,0 +1,11 @@
+#Boss 1
+#Exercise 01
+"""
+🤖 Make a python script that
+read a people name, and show a Welcome message
+according with the typed value.
+"""
+
+name = input("Type your name: ")
+
+print(f"Oh, hello, Sr.{name}. Welcome :)")
---
+++ world1/ex002.py
@@ -0,0 +1,13 @@
+#Boss 1
+#Exercise 02
+"""
+Make a python script that
+read the day, mounth and the year of a person birth
+and, show a message with the date formatted.
+"""
+
+day = input("Day: ")
+mounth = input("Mounth: ")
+year = input("Year: ")
+
+print(f"You were born on this date: {day}/{mounth}/{year}. Right?")
